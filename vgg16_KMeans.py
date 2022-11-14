071cbe9a2ddbba98add608fc1331b971a98469e1
FrederikFritsch <frederik1836@gmail.com>
2022-11-14 08:37:06 +0100

    Removed Fruits Dataset and put old files into OBSOLETE folder.

---
+++ vgg16_KMeans.py
@@ -0,0 +1,20 @@
+# for loading/processing the images  
+from keras.preprocessing.image import load_img 
+from keras.preprocessing.image import img_to_array 
+from keras.applications.vgg16 import preprocess_input 
+
+# models 
+from keras.applications.vgg16 import VGG16 
+from keras.models import Model
+
+# clustering and dimension reduction
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+# for everything else
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from random import randint
+import pandas as pd
+import pickle
