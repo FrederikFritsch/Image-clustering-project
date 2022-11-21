f1d63b595fe2a9ced770d3aeae844f8ed24e3e14
FrederikFritsch <frederik1836@gmail.com>
2022-11-21 14:27:20 +0100

    Cleaned code for running on Alvis

--- ImageClustering.py
+++ ImageClustering.py
@@ -1,5 +1,4 @@
 import numpy as np
-#import cv2 as cv
 import pandas as pd
 import os
 import time
@@ -26,16 +25,15 @@ if __name__ == "__main__":
         normalization_method = str(args[1])
         image_size = (int(args[2]), int(args[2]))
         pca_variance = float(args[3])
-        show_plots = int(args[4])
+        show_plots = False
         min_clusters = int(args[5])
         max_clusters = int(args[6])
-        debug = int(args[7])
+        debug = False
     except Exception as e:
         print("Wrong usage of arguments.")
         print(e)
         quit()
     
-    #image_size = (320, 175)
     take_time = True
     base_dir = os.getcwd()
     full_data_dir_path = base_dir + data_dir
@@ -75,10 +73,10 @@ if __name__ == "__main__":
     
     # Dimensionality reduction
     pca = PCA(pca_variance)
-    #pca.fit(features_df)
+
     features_pca_df = pca.fit_transform(features_df)
-    #print(features_pca_df)
-    if debug: print(f"Explained components: {pca.explained_variance_ratio_}")
+
+    print(f"Explained components: {pca.explained_variance_ratio_}")
 
     # Clustering
     sse, score, silhouette_coefficients, labels = perform_KMeans(features_pca_df, min_clusters, max_clusters)
@@ -92,18 +90,12 @@ if __name__ == "__main__":
     #    n_clusters = kl.elbow + 1
     #else:
     n_clusters = np.argmax(silhouette_coefficients) + min_clusters
-    if debug: print(f"Silhouette coefficient: {n_clusters} clusters return best results")
+    print(f"Silhouette coefficient: {n_clusters} clusters return best results")
     
     lables_index = np.argmax(silhouette_coefficients)
-    #print(lables_index)
-    #print(labels[lables_index])
     cluster_labels = pd.DataFrame(labels[lables_index], columns=["Cluster"])
-    #print(image_names_df)
-    #print(cluster_labels)
-    
-    results_df = pd.concat([image_names_df, cluster_labels], axis=1)
-    #print(results_df)
 
+    results_df = pd.concat([image_names_df, cluster_labels], axis=1)
 
     # Only Plotting below this line
     if show_plots:
@@ -138,56 +130,22 @@ if __name__ == "__main__":
 
 
 
-        #Image merging and showing/storing
-        date = datetime.datetime.now()
-        dir_name = date.strftime("%c")#.replace(":", "")
-        #os.mkdir(base_dir+"/Results/Traditional/"+dir_name)
-        #results_path = base_dir+"/Results/Traditional/"+dir_name+"/"
-        #os.chdir(results_path)
-        for cluster_number in range(n_clusters):
-            cluster = results_df.loc[results_df["Cluster"]==cluster_number]
-            image_list = []
-            for image_path in cluster["Name"]:
-                image_list.append(image_path)
-            column_number = int(np.ceil(np.sqrt(len(image_list))))
-            if len(image_list) > 0:
-                #print(cluster_number)
-                merged_image = combine_images(columns=column_number, space=10, images=image_list)
-                #merged_image.save(str(cluster_number)+".png")
-                merged_image.show()
-        os.chdir(base_dir)
+    #Image merging and showing/storing
+    date = datetime.datetime.now()
+    dir_name = date.strftime("%c")#.replace(":", "")
+    os.mkdir(base_dir+"/Results/Traditional/"+dir_name)
+    results_path = base_dir+"/Results/Traditional/"+dir_name+"/"
+    os.chdir(results_path)
+    for cluster_number in range(n_clusters):
+        cluster = results_df.loc[results_df["Cluster"]==cluster_number]
+        image_list = []
+        for image_path in cluster["Name"]:
+            image_list.append(image_path)
+        column_number = int(np.ceil(np.sqrt(len(image_list))))
+        if len(image_list) > 0:
 
+            merged_image = combine_images(columns=column_number, space=10, images=image_list)
+            merged_image.save(str(cluster_number)+".png")
+            #merged_image.show()
+    os.chdir(base_dir)
 
-
-#Second set - NEED TO DO EDGE DETECTION FOR RGB
-
-#Canny edge
-#edge_canny = cv.Canny(image2, 100, 200)
-#edge_canny = edge_canny.reshape(-1)
-#df['Canny Edge'] = edge_canny
-#
-##Roberts edge
-#edge_roberts = roberts(image)
-#edge_roberts = edge_roberts.reshape(-1)
-#df['Roberts'] = edge_roberts
-#
-##Sobel edge
-#edge_sobel = sobel(image)
-#edge_sobel = edge_sobel.reshape(-1)
-#df['Sobel'] = edge_sobel
-#
-##Scharr edge
-#edge_scharr = scharr(image)
-#edge_scharr = edge_scharr.reshape(-1)
-#df['Scharr'] = edge_scharr
-#
-##Prewitt edge
-#edge_prewitt = prewitt(image)
-#edge_prewitt = edge_prewitt.reshape(-1)
-#df['Prewitt'] = edge_prewitt
-
-#print(df.iloc[:,25])
-#cv.imshow("Filtered Image23", (df.iloc[:,23].values).reshape(grey_image.shape))
-#cv.imshow("Filtered Image22", (df.iloc[:,22].values).reshape(grey_image.shape))
-#print(df.head())
-#cv.waitKey(0)
--- featureExtraction.py
+++ featureExtraction.py
@@ -3,7 +3,6 @@ import numpy as np
 import cv2 as cv
 import pandas as pd
 from scipy.stats import skew
-from Utils import crop_square
 
 
 def traditional_feature_extraction(path, kernels, size = (320, 175)):
@@ -16,49 +15,45 @@ def traditional_feature_extraction(path, kernels, size = (320, 175)):
     
     #Feature 1: Add color distributions as attributes
     for channel, color in zip(cv.split(image), ["Blue", "Green", "Red"]):
-        histogram, bin_edges = np.histogram(channel, bins = 64, range= (0, 256))
-        ##print(histogram)
-        ##print(bin_edges)
+        histogram, bin_edges = np.histogram(channel, bins = 32, range= (0, 256))
         for index, bin in enumerate(bin_edges[0:-1]):
             color_distributions[color+str(bin)] = histogram[index]
         color_distributions[color+"Mean"] = channel.mean()
         color_distributions[color+"Std"] = channel.std()
         color_distributions[color+"Skewness"] = skew(channel.reshape(-1))
-    #cv.waitKey()
-    #print(color_distributions)
 
 
     #Convert to grayscale for gabor filters
-    grey_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY) 
-    image2 = grey_image.reshape(-1)
-    num = 1
-    gabor_features = {}
-
-    for kernel in kernels:
-        gabor_label = 'Gabor' + str(num)
-        fimg = cv.filter2D(image2, cv.CV_8UC3, kernel)
-        filtered_image = np.array(fimg.reshape(-1))
-
-        #Calculate mean and std                    
-        mean = sum(filtered_image)/len(filtered_image)
-        std = np.std(filtered_image)
-
-        #Add mean and std to feature vector
-        gabor_mean_label = gabor_label + "Mean"
-        gabor_std_label = gabor_label + "Std"
-
-        gabor_features[gabor_mean_label] = mean
-        gabor_features[gabor_std_label] = std
-        num += 1
-
-        #df[gabor_label] = filtered_image
-        #print(gabor_label, mean, std)
-        kernel_resized = cv.resize(kernel, (400, 400))
-        #cv.imshow("Kernel: Theta " + str(theta) +" Sigma "+ str(sigma) +" Lamda "+ str(lamda) +" Gamma "+ str(gamma), kernel_resized)
-        #cv.imshow("Kernel", kernel_resized)
-        #cv.imshow("Original img", image)
-        #cv.imshow("Filtered", filtered_image.reshape(grey_image.shape))
-        #cv.waitKey(0)
+    #grey_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY) 
+    #image2 = grey_image.reshape(-1)
+    #num = 1
+    #gabor_features = {}
+#
+    #for kernel in kernels:
+    #    gabor_label = 'Gabor' + str(num)
+    #    fimg = cv.filter2D(image2, cv.CV_8UC3, kernel)
+    #    filtered_image = np.array(fimg.reshape(-1))
+#
+    #    #Calculate mean and std                    
+    #    mean = sum(filtered_image)/len(filtered_image)
+    #    std = np.std(filtered_image)
+#
+    #    #Add mean and std to feature vector
+    #    gabor_mean_label = gabor_label + "Mean"
+    #    gabor_std_label = gabor_label + "Std"
+#
+    #    gabor_features[gabor_mean_label] = mean
+    #    gabor_features[gabor_std_label] = std
+    #    num += 1
+#
+    #    #df[gabor_label] = filtered_image
+    #    #print(gabor_label, mean, std)
+    #    kernel_resized = cv.resize(kernel, (400, 400))
+    #    #cv.imshow("Kernel: Theta " + str(theta) +" Sigma "+ str(sigma) +" Lamda "+ str(lamda) +" Gamma "+ str(gamma), kernel_resized)
+    #    #cv.imshow("Kernel", kernel_resized)
+    #    #cv.imshow("Original img", image)
+    #    #cv.imshow("Filtered", filtered_image.reshape(grey_image.shape))
+    #    #cv.waitKey(0)
 
     #Canny edge
     edge_features = {}
@@ -73,7 +68,7 @@ def traditional_feature_extraction(path, kernels, size = (320, 175)):
     
 
     df1 = pd.DataFrame([color_distributions])
-    df2 = pd.DataFrame([gabor_features])
+    #df2 = pd.DataFrame([gabor_features])
     df3 = pd.DataFrame([edge_features])
     returndf = df1.join(df3)
     return returndf
