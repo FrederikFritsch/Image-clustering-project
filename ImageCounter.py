c10a2e90ca5ec6d8580936150c39b1a070cef466
FrederikFritsch <frederik1836@gmail.com>
2022-11-21 13:58:16 +0100

    Removed images and changed ImageCounter

--- ImageCounter.py
+++ ImageCounter.py
@@ -2,15 +2,22 @@ import os
 
 
 if __name__ == "__main__":
-    
-    data_dir = "/cephyr/NOBACKUP/groups/uu-it-gov"
-    os.chdir(data_dir)
+    print("Hello World")
+    data_dir = "/cephyr/NOBACKUP/groups/uu-it-gov/top20/frames"
     base_dir = os.getcwd()
     print(base_dir)
-    # for index, directories in enumerate(os.walk(full_data_dir_path)):
-    #    for sample in directories[2]:
-    #        if sample.endswith('.png'):
-    #            full_path = directories[0] + "/" + sample
-    #            all_paths.append(full_path)
-
+    tmp = 0
+    for index, directories in enumerate(os.walk(data_dir)):
+        for sample in directories[2]:
+            if sample.endswith('.png'):
+                #full_path = directories[0] + "/" + sample
+                #all_paths.append(full_path)
+                tmp += 1
+    print(tmp)
+    try: 
+        os.mkdir("Results")
+    except:
+        pass
+    results_dir = base_dir + "/Results/"
+    print(results_dir)
     # Store all_paths in file or something
