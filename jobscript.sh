6cddb734dd0fca47d1f105ae4d2a10b8e9b4d40d
Frederik Fritsch <frederik1836@gmail.com>
2022-11-30 10:54:25 +0100

    added variables to jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -9,7 +9,7 @@
 
 #SBATCH -A SNIC2021-7-164      # find your project with the "projinfo" command
 #SBATCH -p alvis               # what partition to use (usually not necessary)
-#SBATCH -t 0-10:00:00          # how long time it will take to run
+#SBATCH -t 0-00:01:00          # how long time it will take to run
 #SBATCH --gpus-per-node=A40:4  # choosing no. GPUs and their type
 #SBATCH -J FirstRun        # the jobname (not necessary)
 
@@ -22,5 +22,7 @@ module load scikit-learn/0.23.2-fosscuda-2020b
 # The rest of this jobscript is handled as a usual bash script that will run
 # on the primary node (in this case there is only one node) of the allocation
 # Here you should make sure to run what you want to be run
+DATAPATH=Image_Data/
+DATAPATH2=/cephyr/NOBACKUP/groups/uu-it-gov/top20/frames
 
-python3 TraditionalFeatureExtraction.py /cephyr/NOBACKUP/groups/uu-it-gov/top20/frames/ normalize 128 0.85 1 20 40 1
+python3 TraditionalFeatureExtraction.py $DATAPATH Test11 244
