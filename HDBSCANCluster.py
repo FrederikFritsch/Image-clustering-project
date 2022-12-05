fd664b19123cb69ba7cb11928793f9e5e2996320
DanloeCHAN <DanloeCHAN@gmail.com>
2022-12-05 05:50:42 +0100

    there are errors in producing labels of clusters

--- HDBSCANCluster.py
+++ HDBSCANCluster.py
@@ -1,11 +1,9 @@
-print("hello this is Lu")
-
 import numpy as np
 import pandas as pd
 import os
 import time
 import sys
-from sklearn.preprocessing import StandardScaler, normalize
+from sklearn.preprocessing import StandardScaler, normalize, MinMaxScaler
 from sklearn.decomposition import PCA
 from src.clusteringAlgorithms import *
 from src.featureExtraction import *
@@ -35,14 +33,32 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
         quit()
-    print(df)
+    #print(df)
+    # Feature normalization
+    features_df = df.loc[:, df.columns[1:]]
+    image_names_df = df.loc[:, ['Name']]
+    
+    # this version using the normalization method and PCA from the one in "KMeansCluster.py"
+    if normalization_method == "normalize":
+        print("Normalizing")
+        scaler = MinMaxScaler()
+        features_df = scaler.fit_transform(features_df)
+    else:
+        scaler = StandardScaler()
+        print("Standardizing")
+        #scaler.fit(features_df)
+        features_df = scaler.fit_transform(features_df)
+    print(features_df)
+    # Dimensionality reduction
+    pca = PCA(pca_variance)
+    
+    features_pca_df = pca.fit_transform(features_df)
 
-    # Here you need to add your code
-    # E.g normalizing, clustering, pca, evaluation etc
-    print("I will have a try today!")
-    print("just to test whether I really know how to connect to Github when I work alone")
-
-
+    print(f"Explained components: {pca.explained_variance_ratio_}")
+    # Clustering algorithm from file "clusteringAlgorithms.py"
+    sse, score, silhouette_coefficients, labels = perform_HDBSCAN(features_pca_df, min_clusters, max_clusters)
+    
+   
 
     os.makedirs(f'{resultspath}', exist_ok=True)  
     #results_df.to_csv(f'{resultspath}/HDBSCANResults.csv') 
