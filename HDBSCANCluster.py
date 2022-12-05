ee8c6c82f7e2a492f6b2cbf74b0cf5c3f95942bd
DanloeCHAN <DanloeCHAN@gmail.com>
2022-12-05 11:03:07 +0100

    add some changes before today's meeting

--- HDBSCANCluster.py
+++ HDBSCANCluster.py
@@ -18,28 +18,30 @@ if __name__ == "__main__":
         filepath = str(args[0])
         resultspath = str(args[1])
         normalization_method = str(args[2])
-        pca_variance = float(args[3])
-        min_clusters = int(args[4])
-        max_clusters = int(args[5])
+        pca_variance = float(args[3])     # how to decide the value of this by ourselves?
+        #min_clusters = int(args[4])       # try several values of min_clusters and max_clusters, the results are the same [-1, -1,...,-1]
+        #max_clusters = int(args[5])
+        min_cluster_size = int(args[4])
+        max_cluster_size = int(args[5])
     except Exception as e:
         print("Wrong usage of arguments.")
         print(e)
         quit()
     
 
-
     try:
         df = pd.read_csv(filepath, index_col=0)
     except Exception as e:
         print(e)
         quit()
-    #print(df)
+    # print(df) # shows one table with image names and features (6*378)
+
     # Feature normalization
-    features_df = df.loc[:, df.columns[1:]]
-    image_names_df = df.loc[:, ['Name']]
+    features_df = df.loc[:, df.columns[1:]]  # get all features
+    image_names_df = df.loc[:, ['Name']]     # get all image names
     
     # this version using the normalization method and PCA from the one in "KMeansCluster.py"
-    if normalization_method == "normalize":
+    if normalization_method == "normalize":      # K-means use "Normalize"
         print("Normalizing")
         scaler = MinMaxScaler()
         features_df = scaler.fit_transform(features_df)
@@ -47,16 +49,15 @@ if __name__ == "__main__":
         scaler = StandardScaler()
         print("Standardizing")
         #scaler.fit(features_df)
-        features_df = scaler.fit_transform(features_df)
+        features_df = scaler.fit_transform(features_df)  # DBSCAN and HDBSCAN use "Standardize" # equal to "StandardScaler().fit_transform(features_df)"
     print(features_df)
-    # Dimensionality reduction
-    pca = PCA(pca_variance)
-    
-    features_pca_df = pca.fit_transform(features_df)
 
+    # Dimensionality reduction
+    pca = PCA(pca_variance)       # try SVD
+    features_pca_df = pca.fit_transform(features_df)
     print(f"Explained components: {pca.explained_variance_ratio_}")
     # Clustering algorithm from file "clusteringAlgorithms.py"
-    sse, score, silhouette_coefficients, labels = perform_HDBSCAN(features_pca_df, min_clusters, max_clusters)
+    sse, score, silhouette_coefficients, labels = perform_HDBSCAN(features_pca_df, min_cluster_size, max_cluster_size)
     
    
 
