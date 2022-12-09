44f364e11a8d94214237b5c4e51f2342dbd66de9
Vlad-Ber <vladik@live.se>
2022-12-09 14:50:26 +0100

    Added Alvis folder

---
+++ Alvis_Scripts/DNNFeatureExt.sh
@@ -0,0 +1,32 @@
+#!/bin/env bash
+
+# This first line is called a "shebang" and indicates what interpreter to use
+# when running this file as an executable. This is needed to be able to submit
+# a text file with `sbatch`
+
+# The necessary flags to send to sbatch can either be included when calling
+# sbatch or more conveniently it can be specified in the jobscript as follows
+
+#SBATCH -A SNIC2021-7-164      # find your project with the "projinfo" command
+#SBATCH -p alvis               # what partition to use (usually not necessary)
+#SBATCH -t 0-48:00:00          # how long time it will take to run
+#SBATCH --gpus-per-node=A40:4  # choosing no. GPUs and their type
+#SBATCH -J VGG16FeatureExctrationFrames        # the jobname (not necessary)
+
+module purge
+module load TensorFlow/2.7.1-foss-2021b-CUDA-11.4.1
+module load Pillow-SIMD/8.3.1-GCCcore-11.2.0
+# The rest of this jobscript is handled as a usual bash script that will run
+# on the primary node (in this case there is only one node) of the allocation
+# Here you should make sure to run what you want to be run
+DATA_PATH=/cephyr/NOBACKUP/groups/uu-it-gov/frames/
+CSV_FOLDER_NAME=DNN-VGG16-Big
+# CURRENTLY SUPPORTED PRE-TRAINED MODELS ARE:
+# - VGG16
+# - XCEPTION
+MODEL_TYPE=VGG16
+#TEST_PATH=/cephyr/NOBACKUP/groups/uu-it-gov/top20/frames/THKgFtr7J2w/
+python3 NeuralNetworkFeatureExtraction.py $DATA_PATH $CSV_FOLDER_NAME $MODEL_TYPE
+
+
+
